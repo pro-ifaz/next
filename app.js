a13cf081427e14ef044cb91072db401f67f6d809
pro-ifaz <ifazahmed98@gmail.com>
2020-12-22 12:40:53 +0600

    footer issue remaining

---
+++ app.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#mobile_menu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+
+menu.addEventListener('click', function(){
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+}
+
+)
